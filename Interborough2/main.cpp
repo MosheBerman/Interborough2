3b81c251340987e523105e0107aee15acef186be
Moshe Berman <moshberm@gmail.com>
2013-05-20 21:46:40 -0400

    Initial Commit

---
+++ Interborough2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Interborough2
+//
+//  Created by Moshe Berman on 5/20/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
