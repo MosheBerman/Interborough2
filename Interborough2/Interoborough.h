cb8f7fd4191bf3eb4220da35a97bdf747f4fd03f
Moshe Berman <moshberm@gmail.com>
2013-05-20 22:50:14 -0400

    Add global header.

---
+++ Interborough2/Interoborough.h
@@ -0,0 +1,20 @@
+//
+//  Interoborough.h
+//  Interborough2
+//
+//  Created by Moshe Berman on 5/20/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Interborough2_Interoborough_h
+#define Interborough2_Interoborough_h
+
+#include <iostream>
+
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl3.h>
+
+#include <string>
+#include <stlib>
+
+#endif
